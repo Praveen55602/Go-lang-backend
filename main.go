5dec55c2b91ce37639b5132ceb18733cabe95a85
Praveen <praveensingh55602@gmail.com>
2024-01-23 00:01:00 +0530

    creating custom server

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	myHttp "net/http" //here myHttp is given as alias to the package
 )
 
-func HelloWorldPage(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "hello world")
+func HelloWorldPage(res myHttp.ResponseWriter, req *myHttp.Request) {
+	res.Header().Set("Content-Type", "text/html") //by default it's text/html only
+	fmt.Fprintf(res, "<h1>hello world</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", HelloWorldPage)
-	http.ListenAndServe("", nil)
+	myHttp.HandleFunc("/", HelloWorldPage)
+	//myHttp.ListenAndServe("", nil) //by default this creates a instance of server and starts listening to the provided port we can create our own server so that we can get more flexibity with the setting of server
+	myCustomServer := myHttp.Server{
+		Addr:         "",
+		Handler:      nil,
+		ReadTimeout:  1000,
+		WriteTimeout: 1000,
+	}
+	myCustomServer.ListenAndServe()
 }
