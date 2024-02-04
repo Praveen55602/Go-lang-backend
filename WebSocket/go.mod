b0944e556ef610646ab40365c9bf533a94ab990e
Praveen <praveensingh55602@gmail.com>
2024-02-04 14:19:13 +0530

    implemented adding multiple clients and broadcasting message to all feature

--- WebSocket/go.mod
+++ WebSocket/go.mod
@@ -3,7 +3,6 @@ module websocket/chatApp
 go 1.21.6
 
 require (
-	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // direct
 	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/text v0.14.0
 )
--- WebSocket/main.go
+++ WebSocket/main.go
@@ -9,14 +9,19 @@ import (
 )
 
 // Creates an instance of the Upgrader struct from the Gorilla WebSocket library. This struct is used to upgrade an HTTP connection to a WebSocket connection.
-var upgrader = websocket.Upgrader{
-	//Sets up a function to allow connections from any origin. In a real application, you might want to implement a security check here.
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
+var (
+	upgrader = websocket.Upgrader{
+		//Sets up a function to allow connections from any origin. In a real application, you might want to implement a security check here.
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	clients = make(map[*websocket.Conn]bool)
+)
 
 // Defines a function to handle WebSocket connections. This function is called when a client requests an upgrade to WebSocket.
+// whenever a new clients calls this function go creates a new separate concurrent instance of this function for that perticular client
 func webSocketHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 
@@ -24,8 +29,12 @@ func webSocketHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer func() {
+		delete(clients, conn)
+		conn.Close()
+	}()
 
+	clients[conn] = true
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
@@ -35,15 +44,20 @@ func webSocketHandler(w http.ResponseWriter, r *http.Request) {
 		//shows recived message from the client
 		log.Printf("receivd message is %s", p)
 
-		//send message to the client
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			fmt.Println(err)
-			return
+		//iterate over all the connected client and sends them the same message except self
+		for client := range clients {
+			if client != conn {
+				err := client.WriteMessage(messageType, p)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
 		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/ws", webSocketHandler)
+	http.HandleFunc("/chat", webSocketHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
